8761b668326302607794f339a04cb9bad9964252
aryanpatel287 <aryanpatel.me@gmail.com>
2026-09-05 10:59:54 +0530

    Initialized React Client, implement dynamic route discovery and navigation registration system for frontend feature modules

---
+++ client/src/app/App.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router';
+
+function App() {
+    return (
+        <div className="app-container">
+            <Outlet />
+        </div>
+    );
+}
+
+export default App;
